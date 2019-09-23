8d4892da30b97c08cd63e1325f5a95e437fbc913
Richard A. Lochner <lochner@clone1.com>
2019-09-23 14:26:14 -0500

    Added tests to verify server is responding to ntp queries.

--- bin/run-sut.sh
+++ bin/run-sut.sh
@@ -5,9 +5,11 @@ set -e
 # run-sut.sh: Script to run system unit tests.
 
 # Wait for chronyd to sync with sources.
-sleep 30s
+sleep 20s
 
 rc="0"
+
+# The following tests query the server to verify that it is running and syncing with its pool servers.
 
 # Query the sources.
 if chronyc -h 10.30.50.2 sources ; then
@@ -22,6 +24,39 @@ if chronyc -h 10.30.50.2 tracking ; then
   echo "The tracking command succeeded!"
 else
   echo "The tracking command failed!"
+  rc="2"
+fi
+
+# The following tests verify that our server is reponding to ntp queries.
+
+# Let's setup the local daemon to query "our" server.
+cat >/etc/chrony.conf <<-__EOF__
+	server 10.30.50.2 iburst
+	driftfile /var/lib/chrony/chrony.drift
+	makestep 1.0 3
+	keyfile /etc/chrony/chrony.keys
+	logdir /var/log/chrony
+	__EOF__
+
+# Start the daemon.
+chronyd -f /etc/chrony.conf
+
+# Wait for it to come up.
+sleep 10s
+
+# Query the local sources.
+if chronyc sources ; then
+  echo "The local sources command succeeded!"
+else
+  echo "The local sources command failed!"
+  rc="1"
+fi
+
+# Query the local tracking stats.
+if chronyc tracking ; then
+  echo "The local tracking command succeeded!"
+else
+  echo "The local tracking command failed!"
   rc="2"
 fi
 
